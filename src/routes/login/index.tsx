1b4733667cbcaa7247bda798b97be382df3e29c8
李国帅 <liguoshuai@enn.cn>
2021-04-27 20:11:49 +0800

    完成登陆页面

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,6 +1,9 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Form, Input, Button } from 'antd';
+import { Form, Input } from 'antd';
+import { UserOutlined, KeyOutlined } from '@ant-design/icons';
+import loginIcon from '@assets/login-icon.png';
+import logoIcon from '@assets/logo-icon.png';
 import { localStorage, axios } from '@utils';
 import { ROUTES } from '@consts';
 import styles from './index.module.scss';
@@ -8,43 +11,70 @@ import styles from './index.module.scss';
 export default function Login() {
   const [form] = Form.useForm();
   const history = useHistory();
-  const login = useCallback(
-    async (values) => {
-      const data = {
-        loginType: 1,
-        appCode: 'easydock',
-        ...values,
-      };
+  const login = useCallback(async () => {
+    const values = await form.validateFields();
+    const data = Object.assign({}, { loginType: 1, appCode: 'easydock' }, values);
+    const loginResponse = await axios.post('/api/auth/v1/login', data, {
+      baseURL: process.env.REACT_APP_LOGIN_DOMAIN,
+    });
 
-      const loginResponse = await axios.post('/api/auth/v1/login', data, {
-        baseURL: process.env.REACT_APP_LOGIN_DOMAIN,
-      });
+    if (loginResponse.data) {
+      localStorage.set('token', loginResponse.data.token);
+      axios.defaults.headers.auth = loginResponse.data.token;
 
-      if (loginResponse.data) {
-        localStorage.set('token', loginResponse.data.token);
-        axios.defaults.headers.auth = loginResponse.data.token;
+      history.replace(ROUTES.INDEX);
+    }
+  }, [history, form]);
 
-        history.replace(ROUTES.INDEX);
-      }
-    },
-    [history],
-  );
+  const nameRules = useMemo(() => {
+    return [
+      {
+        required: true,
+        validator(_: any, value: string) {
+          if (!value) {
+            return Promise.reject(new Error('用户名必填'));
+          }
+
+          return Promise.resolve();
+        },
+      },
+    ];
+  }, []);
+
+  const passwordRules = useMemo(() => {
+    return [
+      {
+        required: true,
+        validator(_: any, value: string) {
+          if (!value) {
+            return Promise.reject(new Error('用户密码必填'));
+          }
+
+          return Promise.resolve();
+        },
+      },
+    ];
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <Form className={styles.form} layout="vertical" form={form} onFinish={login}>
-        <Form.Item label="用户名" name="username" required>
-          <Input placeholder="请输入用户名"></Input>
-        </Form.Item>
-        <Form.Item label="密码" name="password" required>
-          <Input placeholder="请输入密码" type="password"></Input>
-        </Form.Item>
-        <Form.Item className={styles.footer}>
-          <Button htmlType="submit" type="primary">
-            登陆
-          </Button>
-        </Form.Item>
-      </Form>
+    <div className={styles.content}>
+      <img className={styles.image} src={`${process.env.PUBLIC_URL}/images/login.png`} alt="login" />
+      <div className={styles.footer}>
+        <div className={styles.info}>
+          <img className={styles.logo} src={logoIcon} alt="logo" />
+          <div className={styles.welcome}>欢迎使用</div>
+          <div className={styles.name}>低代码平台</div>
+        </div>
+        <Form className={styles.form} layout="vertical" form={form} autoComplete="off">
+          <Form.Item name="username" required rules={nameRules}>
+            <Input placeholder="请输入用户名" prefix={<UserOutlined />} size="large" />
+          </Form.Item>
+          <Form.Item name="password" rules={passwordRules}>
+            <Input placeholder="请输入密码" type="password" prefix={<KeyOutlined />} size="large" />
+          </Form.Item>
+        </Form>
+        <img className={styles['login-icon']} src={loginIcon} alt="login" onClick={login} />
+      </div>
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,5 +6,5 @@ export { default as history } from './history';
 export function getSceneImageUrl(type: keyof typeof SCENE_IAMGES) {
   const publicPath = process.env.PUBLIC_URL;
 
-  return `${publicPath}/scenes/${SCENE_IAMGES[type || 'scene1']}.png`;
+  return `${publicPath}/images/scenes/${SCENE_IAMGES[type || 'scene1']}.png`;
 }
