297802b76e9fc528b4c2a2b2707e5b46f70bd9f2
wangjisi <wangjisi@enn.cn>
2021-05-07 18:42:23 +0800

    restruct project

--- main/src/components/index.tsx
+++ main/src/components/index.tsx
@@ -1,6 +1,6 @@
-export { default as AsyncButton } from "./async-button";
-export { default as Loading } from "./loading";
-export { default as Popover } from "./popover";
-export { default as Popconfirm } from "./popover-confirm";
-export { default as Icon } from "./icon";
-export { default as Text } from "./text";
+export { default as AsyncButton } from './async-button';
+export { default as Loading } from './loading';
+export { default as Popover } from './popover';
+export { default as Popconfirm } from './popover-confirm';
+export { default as Icon } from './icon';
+export { default as Text } from './text';
