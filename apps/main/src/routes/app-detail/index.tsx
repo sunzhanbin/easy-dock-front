14f1b8c70a3763791c518b301e091d736f86ba7f
wangchaochuan <wangcci@enn.cn>
2021-06-08 09:57:24 +0800

    feat:应用详情开发

--- apps/main/src/routes/app-detail/index.tsx
+++ apps/main/src/routes/app-detail/index.tsx
@@ -1,4 +1,4 @@
-import { memo, FC, useEffect, useMemo } from 'react';
+import { memo, FC, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router';
 import { axios } from '@/utils';
@@ -7,18 +7,14 @@ const Container = styled.div``;
 
 const AppDetail: FC = () => {
   const params: { sceneId: string } = useParams();
+  const [subAppList, setSubAppList] = useState<any[]>([]);
   const appId = useMemo(() => params.sceneId, [params]);
   useEffect(() => {
     axios.get(`/subapp/${appId}/list/all`).then((res) => {
-      console.info(res);
+      setSubAppList(res.data);
     });
   }, []);
-  return (
-    <Container>
-      app应用管理
-      <div>1111</div>
-    </Container>
-  );
+  return <Container>{subAppList.length > 0 ? <div>有子应用</div> : <div>没有子应用</div>}</Container>;
 };
 
 export default memo(AppDetail);
