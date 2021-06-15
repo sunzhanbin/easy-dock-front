ca3c7813e54e986f6faabd34908abe309ba4761a
wangjisi <wangjisi@enn.cn>
2021-06-15 10:13:38 +0800

    build package

--- apps/easy-flow/config-overrides.js
+++ apps/easy-flow/config-overrides.js
@@ -41,7 +41,7 @@ module.exports = {
     },
 
     function overrideWebpackPlugin(config) {
-      process.env.REACT_APP_TARGET_ENV === 'development' &&
+      process.env.NODE_ENV === 'development' &&
         config.plugins.push(
           new FileManagerPlugin({
             events: {
