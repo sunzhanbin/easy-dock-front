d8d319465dea77acb7e407b166c5b35556571856
李国帅 <liguoshuai@enn.cn>
2021-06-15 13:20:06 +0800

    给easy-flow添加开发环境的config.js

--- apps/easy-flow/config-overrides.js
+++ apps/easy-flow/config-overrides.js
@@ -41,21 +41,20 @@ module.exports = {
     },
 
     function overrideWebpackPlugin(config) {
-      process.env.NODE_ENV === 'development' &&
-        config.plugins.push(
-          new FileManagerPlugin({
-            events: {
-              onEnd: {
-                copy: [
-                  {
-                    source: `${paths.appPath}/conf.js`,
-                    destination: `${paths.appPublic}/config.js`,
-                  },
-                ],
-              },
+      config.plugins.push(
+        new FileManagerPlugin({
+          events: {
+            onEnd: {
+              copy: [
+                {
+                  source: `${paths.appPath}/conf.js`,
+                  destination: `${paths.appPublic}/config.js`,
+                },
+              ],
             },
-          }),
-        );
+          },
+        }),
+      );
 
       process.env.REACT_APP_TARGET_ENV !== 'development' &&
         config.plugins.push(
