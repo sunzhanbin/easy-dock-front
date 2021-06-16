997090b9b93021b99255f5511d966f594d68f348
李国帅 <liguoshuai@enn.cn>
2021-06-16 09:13:21 +0800

    更换登录为单租户

--- apps/main/conf.js
+++ apps/main/conf.js
@@ -1,6 +1,6 @@
 (function (conf) {
   conf.ALGOR_ORCH_BASE_SERVICE_ENDPOINT = 'http://10.19.151.142:12126';
-  conf.COMMON_LOGIN_DOMAIN = 'http://10.19.151.141:28203';
+  conf.COMMON_LOGIN_DOMAIN = 'http://10.19.151.141:28201';
   conf.COMMON_LOGIN_URL = 'http://localhost:8082/login';
   conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://10.19.151.142:28300';
   conf.ALGOR_ORCH_FRONTEND_ENTRY = 'http://localhost:3000';
