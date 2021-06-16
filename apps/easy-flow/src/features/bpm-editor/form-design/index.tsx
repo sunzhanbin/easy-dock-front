78890c3036f7706e14cc337fe70335de69ecf1ba
wangchaochuan <wangcci@enn.cn>
2021-06-16 18:51:09 +0800

    fix:优化未保存时离开页面确认弹框

--- apps/easy-flow/src/features/bpm-editor/form-design/index.tsx
+++ apps/easy-flow/src/features/bpm-editor/form-design/index.tsx
@@ -11,10 +11,12 @@ import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { dirtySelector, selectedFieldSelector } from './formzone-reducer';
 import { axios } from '@/utils';
 import { Prompt, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
-import { Modal, Popconfirm } from 'antd';
+import { Modal } from 'antd';
+import { ExclamationCircleFilled } from '@ant-design/icons';
 import styles from './index.module.scss';
 import { saveForm } from '@/features/bpm-editor/form-design/formdesign-slice';
 import useMemoCallback from '@common/hooks/use-memo-callback';
+import { AsyncButton } from '@common/components';
 
 const FormDesign: FC<{}> = () => {
   const dispatch = useAppDispatch();
@@ -23,7 +25,6 @@ const FormDesign: FC<{}> = () => {
   const isDirty = useAppSelector(dirtySelector);
   const { bpmId: subAppId } = useParams<{ bpmId: string }>();
   const [isShowTip, setIsShowTip] = useState<boolean>(false);
-  const [saving, setSaving] = useState(false);
   const cancelSaveRef = useRef<boolean>();
   const targetUrlRef = useRef<string>();
   const { url } = useRouteMatch();
@@ -101,9 +102,7 @@ const FormDesign: FC<{}> = () => {
     return true;
   });
   const handleSave = useMemoCallback(async () => {
-    setSaving(true);
     const formResponse = await dispatch(saveForm({ subAppId: subAppId, isShowTip: false, isShowErrorTip: true }));
-    setSaving(false);
     if (formResponse.meta.requestStatus === 'rejected') {
       setIsShowTip(false);
       return;
@@ -112,12 +111,15 @@ const FormDesign: FC<{}> = () => {
       history.push(targetUrlRef.current);
     }
   });
-  const handleCancelUnSaveModal = useMemoCallback((e) => {
+  const handleCancelUnSaveModal = useMemoCallback(() => {
     setIsShowTip(false);
     cancelSaveRef.current = true;
     if (targetUrlRef.current) {
       history.push(targetUrlRef.current);
     }
+  });
+  const handleCloseUnSaveModal = useMemoCallback(() => {
+    setIsShowTip(false);
   });
   return (
     <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart} onDragUpdate={onDragUpdate}>
@@ -127,13 +129,23 @@ const FormDesign: FC<{}> = () => {
         destroyOnClose
         visible={isShowTip}
         width={352}
-        title="提示"
-        okButtonProps={{ size: 'large', loading: saving }}
-        okText="保存更改"
-        cancelButtonProps={{ size: 'large' }}
-        cancelText="放弃保存"
-        onOk={handleSave}
-        onCancel={handleCancelUnSaveModal}
+        title={
+          <div className={styles.tipTitle}>
+            <ExclamationCircleFilled />
+            提示
+          </div>
+        }
+        onCancel={handleCloseUnSaveModal}
+        footer={
+          <>
+            <AsyncButton size="large" onClick={handleCancelUnSaveModal}>
+              放弃保存
+            </AsyncButton>
+            <AsyncButton type="primary" size="large" onClick={handleSave}>
+              保存更改
+            </AsyncButton>
+          </>
+        }
       >
         当前有未保存的更改，您在离开当前页面前是否要保存这些更改?
       </Modal>
