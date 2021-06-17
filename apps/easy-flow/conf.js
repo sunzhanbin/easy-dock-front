17ca2d59035a5450f3e40cf24561e0e1e6b3ffd6
wangjisi <wangjisi@enn.cn>
2021-06-17 11:06:25 +0800

    fix bug of config-overrides.js

--- apps/easy-flow/conf.js
+++ apps/easy-flow/conf.js
@@ -1,5 +1,4 @@
 (function (conf) {
   conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://10.19.151.142:28300';
   conf.COMMON_LOGIN_URL = 'http://localhost:8082/login';
-  conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://10.19.151.142:28300'
 })(window);
--- apps/easy-flow/config-overrides.js
+++ apps/easy-flow/config-overrides.js
@@ -41,20 +41,21 @@ module.exports = {
     },
 
     function overrideWebpackPlugin(config) {
-      config.plugins.push(
-        new FileManagerPlugin({
-          events: {
-            onEnd: {
-              copy: [
-                {
-                  source: `${paths.appPath}/conf.js`,
-                  destination: `${paths.appPublic}/config.js`,
-                },
-              ],
+      process.env.NODE_ENV === 'development' &&
+        config.plugins.push(
+          new FileManagerPlugin({
+            events: {
+              onEnd: {
+                copy: [
+                  {
+                    source: `${paths.appPath}/conf.js`,
+                    destination: `${paths.appPublic}/config.js`,
+                  },
+                ],
+              },
             },
-          },
-        }),
-      );
+          }),
+        );
 
       process.env.REACT_APP_TARGET_ENV !== 'development' &&
         config.plugins.push(
