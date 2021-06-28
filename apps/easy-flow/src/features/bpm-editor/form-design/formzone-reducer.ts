a0c1cde7ee899d073632a9987bfae2703c289dc6
wangchaochuan <wangcci@enn.cn>
2021-06-28 16:56:07 +0800

    fix:新增控件的数据库字段名默认为控件ID

--- apps/easy-flow/src/features/bpm-editor/form-design/formzone-reducer.ts
+++ apps/easy-flow/src/features/bpm-editor/form-design/formzone-reducer.ts
@@ -25,7 +25,7 @@ const reducers = {
         state.layout = [];
       }
       if (state.byId[com.id!]) return state;
-      state.byId[com.id!] = com;
+      state.byId[com.id!] = Object.assign({}, com, { fieldName: com.id });
       // 如果当前选中了某一行，则在当前行之后插入；否则在末尾插入
       if (state.selectedField) {
         const rowNumber = locateById(state.selectedField, state.layout)[0];
