71bfb5c9716627694bf7f96a4cab8e2c0e5982bb
李国帅 <liguoshuai@enn.cn>
2021-07-28 14:26:48 +0800

    更换后端服务地址

--- apps/main/conf.template.js
+++ apps/main/conf.template.js
@@ -1,5 +1,5 @@
 (function (conf) {
-  conf.ALGOR_ORCH_BASE_SERVICE_ENDPOINT = 'http://10.19.151.142:12126';
+  conf.ALGOR_ORCH_BASE_SERVICE_ENDPOINT = 'http://10.19.151.141:12126';
   conf.COMMON_LOGIN_DOMAIN = 'http://10.19.151.141:28201';
   conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://{{external_backend_ip}}:28300';
   conf.ALGOR_ORCH_FRONTEND_ENTRY = 'http://10.19.151.144:28217';
