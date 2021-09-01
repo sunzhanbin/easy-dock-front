a881d6da1cf0bf3a7002a7133f5b5675f6b18c70
李国帅 <liguoshuai@enn.cn>
2021-09-01 15:46:44 +0800

    扩展配置开发

--- apps/easy-flow/src/features/bpm-editor/extend/index.tsx
+++ apps/easy-flow/src/features/bpm-editor/extend/index.tsx
@@ -1,32 +1,34 @@
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
 import { Location } from 'history';
-import { Form, Switch, Checkbox } from 'antd';
+import { Form, Switch, Checkbox, Input } from 'antd';
 import classnames from 'classnames';
-import { MemberSelector, Loading, AsyncButton } from '@common/components';
+import { MemberSelector, Loading, Icon } from '@common/components';
 import useMemoCallback from '@common/hooks/use-memo-callback';
-import { useSubAppDetail, loadExtend, SubAppState, setDirty, setExtend, save } from '@app/app';
+import { useSubAppDetail, loadExtend, SubAppState, setDirty, setExtend } from '@app/app';
 import { TipType } from '@type/subapp';
 import styles from './index.module.scss';
 
 function SubAppExtend() {
   const [form] = Form.useForm<SubAppState['extend']>();
+  const formRef = useRef<typeof form>(form);
   const dispatch = useDispatch();
   const subapp = useSubAppDetail();
-  const handleSubmit = useMemoCallback(async () => {
-    await dispatch(save());
-  });
+
+  useEffect(() => {
+    formRef.current = form;
+  }, [form]);
 
   useEffect(() => {
     if (subapp.data?.id) {
       dispatch(loadExtend(subapp.data.id))
         .then(unwrapResult)
         .then((data) => {
-          form.setFieldsValue(data);
+          formRef.current!.setFieldsValue(data);
         });
     }
-  }, [subapp.data?.id, dispatch, form]);
+  }, [subapp.data?.id, dispatch]);
 
   const handleExtendChange = useMemoCallback((_: any, values: SubAppState['extend']) => {
     dispatch(setDirty(true));
@@ -34,43 +36,67 @@ function SubAppExtend() {
   });
 
   return (
-    <Form form={form} layout="vertical" className={styles.form} onValuesChange={handleExtendChange}>
+    <Form form={form} className={styles.form} onValuesChange={handleExtendChange} colon={false}>
       {subapp.loading && <Loading />}
-      <label className={classnames(styles['horizontal-item'], styles['open-visit'])}>
-        <span className={styles.text}>所有人可访问</span>
-        <Form.Item name={['config', 'openVisit']} noStyle valuePropName="checked">
-          <Switch />
-        </Form.Item>
-      </label>
-
-      <Form.Item label="流程访问权限设置" name="visits">
-        <MemberSelector projectId={subapp.data?.app.project.id} strictDept />
-      </Form.Item>
-
-      <Form.Item label="消息设置">
-        <Form.Item help="当节点有新的待办事项时（包含转交），对该节点的节点负责人进行提醒">
-          <label className={styles['horizontal-item']}>
-            <span className={styles.text}>待办消息</span>
-            <Form.Item valuePropName="checked" name={['config', 'meta', 'messageConfig', 'enableTodo']} noStyle>
-              <Switch />
-            </Form.Item>
-          </label>
-        </Form.Item>
-
-        <Form.Item help="流程结束后，对申请发起人进行申请结果通知，包含：已通过、已拒绝">
-          <label className={styles['horizontal-item']}>
-            <span className={styles.text}>办结消息</span>
-            <Form.Item valuePropName="checked" name={['config', 'meta', 'messageConfig', 'enableDone']} noStyle>
-              <Switch />
-            </Form.Item>
-          </label>
-        </Form.Item>
-      </Form.Item>
 
       <Form.Item
+        className={styles['open-visit-setting']}
+        name={['config', 'openVisit']}
+        label="流程访问权限设置"
+        valuePropName="checked"
+      >
+        <Checkbox>所有人可访问</Checkbox>
+      </Form.Item>
+
+      <Form.Item shouldUpdate noStyle>
+        {({ getFieldValue }) => {
+          const isOpenVisit = getFieldValue(['config', 'openVisit']);
+
+          if (isOpenVisit) return null;
+
+          return (
+            <Form.Item name="visits">
+              <MemberSelector projectId={subapp.data?.app.project.id} strictDept>
+                <>
+                  <Icon className={styles['visits-icon']} type="xinzeng" />
+                  <span className={styles['visits-label']}>添加访问人</span>
+                </>
+              </MemberSelector>
+            </Form.Item>
+          );
+        }}
+      </Form.Item>
+
+      <div className={styles.divider} />
+
+      <h2 className={styles['message-setting__title']}>消息设置</h2>
+
+      <Form.Item
+        className={styles['message-form-item']}
+        label="待办消息"
+        valuePropName="checked"
+        name={['config', 'meta', 'messageConfig', 'enableTodo']}
+      >
+        <Switch />
+      </Form.Item>
+
+      <p className={styles.help}>当节点有新的待办事项时（包含转交），对该节点的节点负责人进行提醒</p>
+
+      <Form.Item
+        className={styles['message-form-item']}
+        label="办结消息"
+        valuePropName="checked"
+        name={['config', 'meta', 'messageConfig', 'enableDone']}
+      >
+        <Switch />
+      </Form.Item>
+
+      <p className={styles.help}>流程结束后，对申请发起人进行申请结果通知，包含：已通过、已拒绝</p>
+
+      <Form.Item
+        className={classnames(styles['message-form-item'], styles['tip-format'])}
         label="提示方式"
         name={['config', 'meta', 'messageConfig', 'noticeChannels']}
-        help="仅对上述的提醒类型生效，自定义提醒的提醒方式需要单独设置"
       >
         <Checkbox.Group>
           <Checkbox value={TipType.WeiChat}>微信提示</Checkbox>
@@ -80,9 +106,38 @@ function SubAppExtend() {
         </Checkbox.Group>
       </Form.Item>
 
-      <AsyncButton className={styles.submit} onClick={handleSubmit} type="primary" size="large">
-        保存
-      </AsyncButton>
+      <p className={styles.help}>仅对上述的提醒类型生效，自定义提醒的提醒方式需要单独设置</p>
+
+      <Form.Item
+        className={styles['webhook-config']}
+        label="webhook设置"
+        name={['config', 'meta', 'webhookConfig', 'enable']}
+        valuePropName="checked"
+      >
+        <Checkbox />
+      </Form.Item>
+
+      <Form.Item shouldUpdate noStyle>
+        {({ getFieldValue }) => {
+          const showUrl = getFieldValue(['config', 'meta', 'webhookConfig', 'enable']);
+          if (!showUrl) return null;
+
+          return (
+            <Form.Item className={styles.webhook} name={['config', 'meta', 'webhookConfig', 'url']}>
+              <Input size="large" placeholder="请输入接口地址" />
+            </Form.Item>
+          );
+        }}
+      </Form.Item>
+
+      <ol className={styles['webhook-help']}>
+        <li className={styles['webhook-help__item']}>
+          1.通过配置Webhook，平台支持在流程流转过程中将流程状态及相关业务数据下发到第三方系统
+        </li>
+        <li className={styles['webhook-help__item']}>
+          2.使用者通过配置接收地址即可接收来自零达下发的流程数据及填报的业务数据（POST请求），从而方便第三方系统的业务处理。
+        </li>
+      </ol>
     </Form>
   );
 }
