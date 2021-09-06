1effa26372ae6865044a61c2ba9afaca0621609f
李国帅 <liguoshuai@enn.cn>
2021-09-06 11:21:44 +0800

    给条件组件传入form

--- apps/easy-flow/src/features/bpm-editor/flow-design/editor/sub-branch/index.tsx
+++ apps/easy-flow/src/features/bpm-editor/flow-design/editor/sub-branch/index.tsx
@@ -49,7 +49,7 @@ function SubBranch(props: SubBranchProps) {
   return (
     <Form form={form} autoComplete="off" layout="vertical" onValuesChange={handleFormValuesChange}>
       <Form.Item name="conditions" label="流转条件">
-        <Condition data={fields} loadDataSource={handleLoadDatasource}></Condition>
+        <Condition form={form} data={fields} loadDataSource={handleLoadDatasource}></Condition>
       </Form.Item>
     </Form>
   );
