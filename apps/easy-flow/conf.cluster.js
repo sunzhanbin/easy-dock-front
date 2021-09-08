34460b3f745f7ae710ee79b5746ea73c4e444ada
wangjisi <wangjisi@enn.cn>
2021-09-08 17:05:18 +0800

    add config for local cluster build

--- apps/easy-flow/conf.cluster.js
+++ apps/easy-flow/conf.cluster.js
@@ -1,4 +1,4 @@
 (function (conf) {
-  conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://backend:28300';
+  conf.EASY_DOCK_BASE_SERVICE_ENDPOINT = 'http://localhost:28300';
   conf.COMMON_LOGIN_URL = 'http://10.19.151.144:28303';
 })(window);
