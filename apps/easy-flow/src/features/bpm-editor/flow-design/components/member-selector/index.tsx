936d23f669c269d730c6f3dd17992f268bd9aafa
李国帅 <liguoshuai@enn.cn>
2021-09-15 10:22:01 +0800

    动态组件开发

--- apps/easy-flow/src/features/bpm-editor/flow-design/components/member-selector/index.tsx
+++ apps/easy-flow/src/features/bpm-editor/flow-design/components/member-selector/index.tsx
@@ -11,23 +11,38 @@ interface MemberSelectorProps {
 }
 
 function MemberSelector(props: MemberSelectorProps) {
-  const { cacheMembers } = useAppSelector(flowDataSelector);
+  const { cacheMembers, fieldsTemplate } = useAppSelector(flowDataSelector);
   const { value, onChange } = props;
   const { data: subAppDetail } = useSubAppDetail();
   const dispatch = useAppDispatch();
+  const dynamicFields = useMemo(() => {
+    return fieldsTemplate
+      .filter((field) => field.type === 'Member')
+      .map((field) => ({ name: field.name, key: field.id }));
+  }, [fieldsTemplate]);
+
   const showValue: NonNullable<SelectorProps['value']> = useMemo(() => {
-    const { members = [], depts = [], roles = [] } = value!;
+    const { members = [], depts = [], roles = [], dynamic } = value!;
+    const fieldsMap = (dynamic?.fields || []).reduce((curr, next) => {
+      curr[next] = true;
+      return curr;
+    }, {} as { [key: string]: true });
 
     return {
       depts: depts.map((id) => ({ ...cacheMembers[id] })),
       members: members.map((id) => ({ ...cacheMembers[id] })),
       roles: roles.map((id) => ({ ...cacheMembers[id] })),
+      dynamic: {
+        starter: dynamic?.starter,
+        roles: (dynamic?.roles || []).map((id) => ({ ...cacheMembers[id] })),
+        fields: dynamicFields.filter((field) => fieldsMap[field.key]),
+      },
     };
-  }, [value, cacheMembers]);
+  }, [value, cacheMembers, dynamicFields]);
 
   const handleChange = (value: NonNullable<SelectorProps['value']>) => {
     const caches: Parameters<typeof setCacheMembers>[number] = {};
-    const { members = [], depts = [], roles = [] } = value;
+    const { members = [], depts = [], roles = [], dynamic } = value;
     const memberIds: CorrelationMemberConfigKey[] = [];
     const deptIds: CorrelationMemberConfigKey[] = [];
     const roleIds: CorrelationMemberConfigKey[] = [];
@@ -42,7 +57,7 @@ function MemberSelector(props: MemberSelectorProps) {
       deptIds.push(dept.id);
     });
 
-    roles.forEach((role) => {
+    roles.concat(dynamic?.roles || []).forEach((role) => {
       caches[role.id] = role;
       roleIds.push(role.id);
     });
@@ -54,11 +69,24 @@ function MemberSelector(props: MemberSelectorProps) {
         members: memberIds,
         depts: depts.map((depart) => depart.id),
         roles: roles.map((role) => role.id),
+        dynamic: {
+          starter: dynamic?.starter,
+          roles: (dynamic?.roles || []).map((role) => role.id),
+          fields: (dynamic?.fields || []).map((field) => field.key),
+        },
       });
     }
   };
 
-  return <Selector projectId={subAppDetail?.app.project.id} value={showValue} onChange={handleChange} strictDept />;
+  return (
+    <Selector
+      projectId={subAppDetail?.app.project.id}
+      value={showValue}
+      onChange={handleChange}
+      strictDept
+      dynamicFields={dynamicFields}
+    />
+  );
 }
 
 export default memo(MemberSelector);
