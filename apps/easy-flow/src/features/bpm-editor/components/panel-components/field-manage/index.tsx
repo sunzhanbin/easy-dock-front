33f7cd66b6130532362a4316824b6d4049b2b5e3
wangchaochuan <wangcci@enn.cn>
2021-10-18 09:25:45 +0800

    feat:对接单点登录

--- apps/easy-flow/src/features/bpm-editor/components/panel-components/field-manage/index.tsx
+++ apps/easy-flow/src/features/bpm-editor/components/panel-components/field-manage/index.tsx
@@ -1,6 +1,8 @@
+import { memo, useMemo } from 'react';
+import { Dropdown, Menu } from 'antd';
 import { Icon } from '@common/components';
 import useMemoCallback from '@common/hooks/use-memo-callback';
-import { memo } from 'react';
+import componentSchema from '@/config/components';
 import styles from './index.module.scss';
 
 interface ComProps {
@@ -8,16 +10,40 @@ interface ComProps {
   onChange?: (value: this['value']) => void;
 }
 
+const componentList = Object.values(componentSchema)
+  .map((com) => com.baseInfo)
+  .filter((com) => com.type !== 'Tabs');
+
 const FieldManage = (props: ComProps) => {
-  const addItem = useMemoCallback(() => {
-    console.info('add');
+  const handleAddComponent = useMemoCallback((type: string) => {
+    console.info(type, 'add');
   });
+  const overlay = useMemo(() => {
+    return (
+      <Menu>
+        {componentList.map(({ type, name }) => (
+          <Menu.Item
+            key={type}
+            className={styles.item}
+            onClick={() => {
+              handleAddComponent(type);
+            }}
+          >
+            {name}
+          </Menu.Item>
+        ))}
+      </Menu>
+    );
+  }, [handleAddComponent]);
+
   return (
     <div className={styles.fields}>
-      <div className={styles.add_custom} onClick={addItem}>
-        <Icon className={styles.iconfont} type="xinzengjiacu" />
-        <span>添加字段</span>
-      </div>
+      <Dropdown placement="bottomLeft" trigger={['click']} overlay={overlay} getPopupContainer={(c) => c}>
+        <div className={styles.add_custom}>
+          <Icon className={styles.iconfont} type="xinzengjiacu" />
+          <span>添加字段</span>
+        </div>
+      </Dropdown>
     </div>
   );
 };
