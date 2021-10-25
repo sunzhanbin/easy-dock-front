034980d200e04da72ee2ad84b1c9bad862b1ce31
wangchaochuan <wangcci@enn.cn>
2021-10-25 16:49:54 +0800

    feat:单点登录

--- apps/main/src/index.js
+++ apps/main/src/index.js
@@ -1,6 +1,6 @@
 (async () => {
   const token = await window.Auth.getToken(true, window.EASY_DOCK_BASE_SERVICE_ENDPOINT)
   if(token) {
-    require('./index.tsx')
+    require('./app.tsx')
   }
 })()
