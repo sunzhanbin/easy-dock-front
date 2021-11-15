d3011548cb7f22ee1c8107875fe7d0ad55dcb6c4
卫徕 <weilaib@enn.cn>
2021-11-15 13:25:53 +0800

    docs: 生成文档

--- packages/sso/src/index.ts
+++ packages/sso/src/index.ts
@@ -1,7 +1,14 @@
+import Cookies from 'js-cookie';
+
+/** @ignore */ 
 interface IAuth {
+    /** */
     getToken: (needAutoLogin: boolean, host: string) => Promise<unknown>;
+    /** */
     getAuth: () => string | null;
+    /** */
     removeAuth: () => void;
+    /** */
     logout: (redirect: string) => void;
 }
 
@@ -55,13 +62,17 @@ const fetchToken: (host: string, code: string) => Promise<string> = async (host,
     }
 };
 
+/**
+ * @class
+ */
 class Auth implements IAuth {
     appId: string;
     server: string | undefined;
-    coodAttr: string = DEFAULT_CODE_ATTR;
+    codeAttr: string = DEFAULT_CODE_ATTR;
     authAttr: string = DEFAULT_AUTH_ATTR;
+    cookieAttr?: string;
 
-    constructor(server?: string, coodAttr?: string) {
+    constructor(server?: string, codeAttr?: string) {
         const currentScript: HTMLOrSVGScriptElementExt | null = document.currentScript as HTMLOrSVGScriptElementExt;
 
         let appId = currentScript?.getAttribute('appId');
@@ -90,21 +101,36 @@ class Auth implements IAuth {
             }
         }
 
-        if (coodAttr) {
-            this.coodAttr = coodAttr;
+        if (codeAttr) {
+            this.codeAttr = codeAttr;
         }
     }
 
-    async getToken(needAutoLogin: boolean, host: string) {
+    private setAuth(token: string) {
+        if (this.cookieAttr) {
+            Cookies.set(this.cookieAttr, token);
+        }
+
+        window.localStorage.setItem(this.authAttr, token);
+    }
+
+    /**
+     * @description 获取Token
+     * @param {boolean} needAutoLogin
+     * @param {string} host BASE_SERVICE_ENDPOINT
+     * @returns Promise<null>
+     */
+    public async getToken(needAutoLogin: boolean, host: string) {
         if (this.getAuth()) {
             return this.getAuth();
         } else {
-            let code = getParams()[this.coodAttr];
+            let code = getParams()[this.codeAttr];
             if (code) {
                 let token = await fetchToken(host, code);
                 if (token) {
-                    window.localStorage.setItem(this.authAttr, token);
-                    resetUrl(this.coodAttr);
+                    // window.localStorage.setItem(this.authAttr, token);
+                    this.setAuth(token);
+                    resetUrl(this.codeAttr);
                 }
                 return token;
             } else {
@@ -129,37 +155,62 @@ class Auth implements IAuth {
         }
     }
 
-    setLoginServer(server: string) {
+    /**
+     *
+     * @param server {string} 登陆接口地址
+     */
+    public setLoginServer(server: string) {
         this.server = server;
     }
 
-    setConfig({ server, appId, coodAttr, authAttr }: { server?: string; appId?: string; coodAttr?: string; authAttr?: string }) {
+    /**
+     *
+     * @param {object} config
+     *  server: 登陆接口地址, appId: appId, codeAttr: 在localStorage设置code使用的Key , authAttr: 在localStorage设置auth使用的Key
+     */
+    public setConfig({ server, appId, codeAttr, authAttr, cookieAttr }: { server?: string; appId?: string; codeAttr?: string; authAttr?: string; cookieAttr?: string }) {
         if (server) {
-            this.server = server;
+            this.setLoginServer(server);
         }
 
         if (appId) {
             this.appId = appId;
         }
 
-        if (coodAttr) {
-            this.coodAttr = coodAttr;
+        if (codeAttr) {
+            this.codeAttr = codeAttr;
         }
 
         if (authAttr) {
             this.authAttr = authAttr;
         }
+
+        if (cookieAttr) {
+            this.cookieAttr = cookieAttr;
+        }
     }
 
-    getAuth() {
+    public getAuthFromCookie() {
+        if (this.cookieAttr) {
+            return Cookies.get(this.cookieAttr);
+        }
+
+        return null;
+    }
+
+    public getAuth() {
         return window.localStorage.getItem(this.authAttr);
     }
 
-    removeAuth() {
+    public removeAuth() {
         window.localStorage.removeItem(this.authAttr);
+
+        if (this.cookieAttr) {
+            Cookies.remove(this.cookieAttr);
+        }
     }
-    
-    logout(redirect: string) {
+
+    public logout(redirect: string) {
         let auth = this.getAuth();
         if (auth) {
             this.removeAuth();
