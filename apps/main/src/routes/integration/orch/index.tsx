22eaadce5292c2fa17b9a44f370217c2923c1ac8
卫徕 <weilaib@enn.cn>
2021-11-18 16:19:19 +0800

    fix: 子应用渲染失败的情况下，主应用不崩溃。

--- apps/main/src/routes/integration/orch/index.tsx
+++ apps/main/src/routes/integration/orch/index.tsx
@@ -4,6 +4,7 @@ import { loadMicroApp } from 'qiankun';
 import Auth from '@enc/sso';
 
 import Loading from '@components/loading';
+
 import classnames from 'classnames';
 import useMatchRoute from '@hooks/use-match-route';
 import { envs } from '@consts';
@@ -32,17 +33,22 @@ export default function Orch() {
       {
         sandbox: {
           // 严格隔离样式会导致icon失效, 暂时关闭, TODO: 优化
+          // 2021/11/18 weilaib: 已有解决方案，可以设置为 true
           strictStyleIsolation: false,
         },
       },
     );
 
-    orch.mountPromise.finally(() => {
-      // 防止页面跳走后才加载成功时setstate的警告;
-      if (orchRef.current) {
-        setLoading(false);
-      }
-    });
+    orch.mountPromise
+      .catch((e) => {
+        console.error('loadMicroApp envs.ALGOR_ORCH_FRONTEND_ENTRY error:', e);
+      })
+      .finally(() => {
+        // 防止页面跳走后才加载成功时setstate的警告;
+        if (orchRef.current) {
+          setLoading(false);
+        }
+      });
 
     return () => {
       orch.unmount();
