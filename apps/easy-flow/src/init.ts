ca5061e6d43e1b71c428a60a5251dd48086ea0f1
wangjisi <wangjisi@enn.cn>
2021-11-24 16:38:55 +0800

    add log to view webpack_piublic_path

--- apps/easy-flow/src/init.ts
+++ apps/easy-flow/src/init.ts
@@ -10,6 +10,7 @@ if (window.__POWERED_BY_QIANKUN__) {
   appConfig.micro = true;
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__; // 替换webpack的ouput.publicPath
+  console.log(`__webpack_public_path__: ${__webpack_public_path__}`);
   // eslint-disable-next-line no-undef
   appConfig.publicPath = __webpack_public_path__; // 加载public文件夹里的资源使用
 }
