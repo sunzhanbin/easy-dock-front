95cc23ef252917ded32d1c4edbb65512259b5fb0
lida <lidah@enn.cn>
2021-11-30 20:28:05 +0800

    feat: 菜单列表表单状态管理

--- apps/entry/src/containers/app-setup-config/menu-setup-list.component.tsx
+++ apps/entry/src/containers/app-setup-config/menu-setup-list.component.tsx
@@ -2,26 +2,53 @@ import { useCallback } from "react";
 import { Button } from "antd";
 import { PlusOutlined, MenuOutlined, RestOutlined } from "@ant-design/icons";
 import { useAppDispatch, useAppSelector } from "@/store";
-import { selectMenu, add } from "@/views/app-setup/menu-setup.slice";
+import {
+  selectMenu,
+  selectCurrentId,
+  setCurrentMenu,
+  add,
+  remove,
+} from "@/views/app-setup/menu-setup.slice";
 import { v4 as uuid } from "uuid";
+import classnames from "classnames";
 import "./menu-setup-list.style";
 
+// 菜单单元组件；
 const MenuItemComponent = ({ menu }: any) => {
   const dispatch = useAppDispatch();
+  const currentId = useAppSelector(selectCurrentId);
 
   const handleAddMenu = useCallback((currentId: string) => {
     const childId = uuid();
     dispatch(add({ currentId, childId }));
   }, []);
 
+  const handleRemoveMenu = useCallback((currentId: string) => {
+    dispatch(remove(currentId));
+  }, []);
+
+  const handleMenuClick = useCallback((currentId: string) => {
+    dispatch(setCurrentMenu(currentId));
+  }, []);
+
   return (
-    <div className="menu-item">
-      <span className="text">{menu.name}</span>
+    <div
+      className={classnames({
+        "menu-item": true,
+        active: currentId == menu.id,
+      })}
+    >
+      <span className="text" onClick={handleMenuClick.bind(null, menu.id)}>
+        {menu.name}
+      </span>
       <span className="acts">
         <span className="item add" onClick={handleAddMenu.bind(null, menu.id)}>
           <PlusOutlined />
         </span>
-        <span className="item remove">
+        <span
+          className="item remove"
+          onClick={handleRemoveMenu.bind(null, menu.id)}
+        >
           <RestOutlined />
         </span>
         <span className="item drag">
@@ -32,6 +59,7 @@ const MenuItemComponent = ({ menu }: any) => {
   );
 };
 
+// 菜单嵌套逻辑组件；
 const MenuComponent = ({ menu }: any) => {
   return (
     <>
@@ -51,6 +79,7 @@ const MenuComponent = ({ menu }: any) => {
   );
 };
 
+// 菜单容器组件；
 const MenuSetupListComponent = () => {
   const dispatch = useAppDispatch();
   const menu = useAppSelector(selectMenu);
