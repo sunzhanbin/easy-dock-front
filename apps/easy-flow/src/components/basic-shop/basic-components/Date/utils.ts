1644f49857c126d5d38435fe75f59848246db95c
caox <caox@enn.cn>
2021-12-01 14:14:10 +0800

    chore: 日期控件不早于问题修改

--- apps/easy-flow/src/components/basic-shop/basic-components/Date/utils.ts
+++ apps/easy-flow/src/components/basic-shop/basic-components/Date/utils.ts
@@ -26,9 +26,16 @@ const analyseRangeRule = (
   compareTimeResult: boolean,
   range: { [key: string]: any },
 ) => {
-  if (range && (!currentTime || compareTimeResult)) {
+  if (range && currentTime && compareTimeResult) {
     // 属性面板日期配置
-    return (current && current < moment(range.min)) || (current && current > moment(range.max));
+    let rule1, rule2;
+    if (range.min) {
+      rule1 = current && current < moment(range.min);
+    }
+    if (range.max) {
+      rule2 = current && current > moment(range.max);
+    }
+    return rule1 || rule2;
   }
 };
 
@@ -41,44 +48,76 @@ const analyseRangeRule = (
  * @param range 属性面板日期范围
  */
 const getDisabledDateRule = ({ rules, current, formValue, id, range }: RuleParams): boolean => {
-  let rules1, rules2, rules3, rules4, rules5;
+  let rules1, rules2, rules3, rules4, rules5, rule6, rule7;
   (rules ?? []).forEach((item) => {
     const { watch, condition } = item;
     if (condition.symbol === 'earlier') {
+      // 不晚于
       if (Object.keys(flowVarsMap).includes(watch[0])) {
         const currentTime = getFlowVarsRule(watch[0])!;
-        rules5 = analyseRangeRule(current, currentTime, currentTime > moment(range?.min).valueOf(), range!);
+        rules5 = analyseRangeRule(
+          current,
+          currentTime,
+          currentTime > moment(range?.max).valueOf() || currentTime > moment(range?.min).valueOf(),
+          range!,
+        );
         // 选择流程变量当前时间等
         rules1 = current.valueOf() > currentTime;
       } else {
         // 选择其他控件
         if (id === condition.fieldName) {
           const currentTime = formValue[condition.value as string];
-          rules5 = analyseRangeRule(current, currentTime, currentTime > moment(range?.min).valueOf(), range!);
+          rules5 = analyseRangeRule(
+            current,
+            currentTime,
+            currentTime > moment(range?.min).valueOf() || currentTime > moment(range?.max).valueOf(),
+            range!,
+          );
           rules3 = current.valueOf() > currentTime;
         } else if (id === condition.value) {
           const currentTime = formValue[condition.fieldName as string];
-          rules5 = analyseRangeRule(current, currentTime, currentTime < moment(range?.max).valueOf(), range!);
+          rules5 = analyseRangeRule(
+            current,
+            currentTime,
+            currentTime < moment(range?.max).valueOf() || currentTime < moment(range?.min).valueOf(),
+            range!,
+          );
           rules3 = current.valueOf() < currentTime;
         }
       }
     }
 
     if (condition.symbol === 'latter') {
+      // 不早于
       if (Object.keys(flowVarsMap).includes(watch[0])) {
         const currentTime = getFlowVarsRule(watch[0])!;
-        rules5 = analyseRangeRule(current, currentTime, currentTime < moment(range?.max).valueOf(), range!);
+        rules5 = analyseRangeRule(
+          current,
+          currentTime,
+          currentTime < moment(range?.min).valueOf() || currentTime < moment(range?.max).valueOf(),
+          range!,
+        );
         // 选择流程变量当前时间等
         rules2 = current.valueOf() < currentTime;
       } else {
         // 选择其他控件
         if (id === condition.fieldName) {
           const currentTime = formValue[condition.value as string];
-          rules5 = analyseRangeRule(current, currentTime, currentTime < moment(range?.min).valueOf(), range!);
+          rules5 = analyseRangeRule(
+            current,
+            currentTime,
+            currentTime < moment(range?.min).valueOf() || currentTime < moment(range?.max).valueOf(),
+            range!,
+          );
           rules4 = current.valueOf() < currentTime;
         } else if (id === condition.value) {
           const currentTime = formValue[condition.fieldName as string];
-          rules5 = analyseRangeRule(current, currentTime, currentTime > moment(range?.min).valueOf(), range!);
+          rules5 = analyseRangeRule(
+            current,
+            currentTime,
+            currentTime > moment(range?.min).valueOf() || currentTime > moment(range?.max).valueOf(),
+            range!,
+          );
           rules4 = current.valueOf() > currentTime;
         }
       }
@@ -86,12 +125,19 @@ const getDisabledDateRule = ({ rules, current, formValue, id, range }: RuleParam
   });
 
   if (!rules && range) {
-    rules5 =
-      (range.min && current.valueOf() < moment(range.min).valueOf()) ||
-      (range.max && current.valueOf() > moment(range.max).valueOf());
+    console.log('comrin');
+    if (range.min) {
+      rule6 = current && current < moment(range.min);
+    }
+    if (range.max) {
+      rule7 = current && current > moment(range.max);
+    }
+    // rules5 =
+    //   (range.min && current.valueOf() < moment(range.min).valueOf()) ||
+    //   (range.max && current.valueOf() > moment(range.max).valueOf());
   }
   // @ts-ignore
-  return rules1 || rules2 || rules3 || rules4 || rules5;
+  return rules1 || rules2 || rules3 || rules4 || rules5 || rule6 || rule7;
 };
 
 export default getDisabledDateRule;
