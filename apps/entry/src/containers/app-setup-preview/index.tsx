883e0bf7e9fece440c7a9957d2a6a14b81a2afbc
lida <lidah@enn.cn>
2021-12-04 18:56:39 +0800

    feat: 编排区优化结构

--- apps/entry/src/containers/app-setup-preview/index.tsx
+++ apps/entry/src/containers/app-setup-preview/index.tsx
@@ -1,42 +1,11 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useAppSelector } from "@/store";
-import { NavMenuComponentProps } from "@utils/types";
 import { selectTheme, selectNavMode } from "@views/app-setup/basic-setup.slice";
 import { selectMenu, selectCurrentId } from "@views/app-setup/menu-setup.slice";
 import SingleNavComponent from "@containers/app-setup-preview/single-nav.component";
 import MultiNavComponent from "@containers/app-setup-preview/multi-nav.component";
 import { SingleNavAppInfo, MultiNavAppInfo } from "@components/app-info";
 import "@containers/app-setup-preview/index.style";
-
-const NavMenuComponent = ({
-  navMode,
-  selectedKey,
-  dataSource,
-  children,
-}: NavMenuComponentProps) => {
-  return (
-    <div className="nav-menu-component">
-      {navMode === "single" && (
-        <SingleNavComponent
-          selectedKey={selectedKey}
-          dataSource={dataSource}
-          extra={<SingleNavAppInfo />}
-        >
-          {children}
-        </SingleNavComponent>
-      )}
-      {navMode === "multi" && (
-        <MultiNavComponent
-          selectedKey={selectedKey}
-          dataSource={dataSource}
-          extra={<MultiNavAppInfo />}
-        >
-          {children}
-        </MultiNavComponent>
-      )}
-    </div>
-  );
-};
 
 const AppSetupPreview = () => {
   const theme = useAppSelector(selectTheme);
@@ -52,15 +21,30 @@ const AppSetupPreview = () => {
     );
   }, [theme, navMode]);
 
+  const renderContent = useCallback(() => {
+    return <>这里是内容区</>;
+  }, []);
+
   return (
     <div className="app-setup-preview">
-      <NavMenuComponent
-        selectedKey={selectedKey}
-        dataSource={menu}
-        navMode={navMode}
-      >
-        这里是内容区
-      </NavMenuComponent>
+      {navMode === "single" && (
+        <SingleNavComponent
+          selectedKey={selectedKey}
+          dataSource={menu}
+          extra={<SingleNavAppInfo />}
+        >
+          {renderContent()}
+        </SingleNavComponent>
+      )}
+      {navMode === "multi" && (
+        <MultiNavComponent
+          selectedKey={selectedKey}
+          dataSource={menu}
+          extra={<MultiNavAppInfo />}
+        >
+          {renderContent()}
+        </MultiNavComponent>
+      )}
     </div>
   );
 };
