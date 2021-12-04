b0a52e45b3db8e00de25965fa24bf33194075ded
wangchaochuan <wangcci@enn.cn>
2021-12-04 11:00:24 +0800

    fix:修复bug编号8823

--- apps/easy-flow/src/components/basic-shop/basic-components/DescText/index.tsx
+++ apps/easy-flow/src/components/basic-shop/basic-components/DescText/index.tsx
@@ -1,9 +1,11 @@
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import styles from './index.module.scss';
 
 const DescText = (props: { value: string; text_value?: string }) => {
   const { value, text_value } = props;
-  const descValue = value || text_value;
+  const descValue = useMemo(() => {
+    return value || text_value;
+  }, [value, text_value]);
   const renderContent = useCallback(() => {
     if (!descValue || descValue.trim() === '<p></p>') {
       return <div className={styles.desc}>请补充描述性文字</div>;
