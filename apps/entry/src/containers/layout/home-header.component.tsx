91154c236a3bf1a8ec7681d48167260dc85a772f
caox <caox@enn.cn>
2021-12-08 17:46:06 +0800

    chore: header样式修改

--- apps/entry/src/containers/layout/home-header.component.tsx
+++ apps/entry/src/containers/layout/home-header.component.tsx
@@ -1,35 +1,46 @@
 import React from "react";
+import classnames from "classnames";
 import { NavLink } from "react-router-dom";
 import Header from "@components/header";
 import "@containers/layout/home-header.style";
 
 const HomeHeader: React.FC = () => {
-  const activeStyle = {
-    textDecoration: "underline",
-  };
   return (
     <div className="home_header">
       <Header>
         <nav className="nav_menu">
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                style={({ isActive }) => (isActive ? activeStyle : {})}
-              >
-                开始
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/asset-centre">资产中心</NavLink>
-            </li>
-            <li>
-              <NavLink to="/app-manager">应用管理</NavLink>
-            </li>
-            <li>
-              <NavLink to="/template-mall">模板商城</NavLink>
-            </li>
-          </ul>
+          <NavLink
+            className={({ isActive }) =>
+              classnames({ nav: true, active: isActive })
+            }
+            to="/"
+          >
+            开始
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              classnames({ nav: true, active: isActive })
+            }
+            to="/asset-centre"
+          >
+            资产中心
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              classnames({ nav: true, active: isActive })
+            }
+            to="/app-manager"
+          >
+            应用管理
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              classnames({ nav: true, active: isActive })
+            }
+            to="/template-mall"
+          >
+            模板商城
+          </NavLink>
         </nav>
       </Header>
     </div>
