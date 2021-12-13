2ca930444c2435d91ea939858eec179acb83591b
wangchaochuan <wangcci@enn.cn>
2021-12-13 10:14:39 +0800

    feat:空状态下创建子应用功能

--- apps/entry/src/containers/app-manager-details/app-empty.component.tsx
+++ apps/entry/src/containers/app-manager-details/app-empty.component.tsx
@@ -1,23 +1,43 @@
-import { memo, FC, ReactNode, useMemo } from "react";
+import { memo, FC, ReactNode, useMemo, useState } from "react";
+import { message, Button } from "antd";
 import { SubAppType } from "@/consts";
+import { Icon } from "@common/components";
+import useMemoCallback from "@common/hooks/use-memo-callback";
+import { useAppSelector } from "@/store";
+import SpaceImage from "@assets/images/space.png";
+import { selectCurrentWorkspaceId } from "@/views/app-manager/index.slice";
+import { useCreateSupAppMutation } from "@/http/app-manager.hooks";
 import FormImage from "@assets/images/form.png";
 import FlowImage from "@assets/images/flow.png";
 import ChartImage from "@assets/images/chart.png";
 import CanvasImage from "@assets/images/canvas.png";
-import SpaceImage from "@assets/images/space.png";
-import "./app-empty.style.scss";
-import { Icon } from "@common/components";
-import { Button } from "antd";
 import AppModal from "./app-modal.component";
+import "./app-empty.style.scss";
 
 interface TypeItem {
   type: SubAppType;
   title: string;
   desc: string;
   children: ReactNode;
+  showModal: boolean;
 }
 
+type ShowModalMap = {
+  [k in SubAppType]: boolean;
+};
+
+const initialShowModal: ShowModalMap = {
+  [SubAppType.FORM]: false,
+  [SubAppType.FLOW]: false,
+  [SubAppType.CHART]: false,
+  [SubAppType.CANVAS]: false,
+  [SubAppType.SPACE]: false,
+};
+
 const AppEmpty: FC = () => {
+  const workspaceId = useAppSelector(selectCurrentWorkspaceId);
+  const [createSubApp] = useCreateSupAppMutation();
+  const [showModal, setShowModal] = useState<ShowModalMap>(initialShowModal);
   const typeList = useMemo<TypeItem[]>(() => {
     return [
       {
@@ -25,36 +45,53 @@ const AppEmpty: FC = () => {
         title: "新建表单",
         desc: "配置表单、列表",
         children: <img src={FormImage} alt="图片" className="image" />,
+        showModal: showModal[SubAppType.FORM],
       },
       {
         type: SubAppType.FLOW,
         title: "新建流程",
         desc: "配置流程",
         children: <img src={FlowImage} alt="图片" className="image" />,
+        showModal: showModal[SubAppType.FLOW],
       },
       {
         type: SubAppType.CHART,
         title: "新建报表",
         desc: "配置业务统计报表",
         children: <img src={ChartImage} alt="图片" className="image" />,
+        showModal: showModal[SubAppType.CHART],
       },
       {
         type: SubAppType.CANVAS,
         title: "新建大屏",
         desc: "配置可视化大屏",
         children: <img src={CanvasImage} alt="图片" className="image" />,
+        showModal: showModal[SubAppType.CANVAS],
       },
       {
         type: SubAppType.SPACE,
         title: "新建空间",
         desc: "配置3D空间、动画等",
         children: <img src={SpaceImage} alt="图片" className="image" />,
+        showModal: showModal[SubAppType.SPACE],
       },
     ];
-  }, []);
+  }, [showModal]);
+  const handleShowModal = useMemoCallback((type: SubAppType) => {
+    const showModal: ShowModalMap = { ...initialShowModal, [type]: true };
+    setShowModal(showModal);
+  });
+  const handleClose = useMemoCallback(() => {
+    setShowModal(initialShowModal);
+  });
+  const handleOk = useMemoCallback((name: string, type: SubAppType) => {
+    createSubApp({ appId: workspaceId, name, type }).then(() => {
+      message.success("子应用创建成功!");
+    });
+  });
   return (
     <div className="app-empty">
-      {typeList.map(({ type, title, desc, children }) => {
+      {typeList.map(({ type, title, desc, children, showModal }) => {
         return (
           <div className="empty-item" key={type}>
             <div className="title">{title}</div>
@@ -67,8 +104,18 @@ const AppEmpty: FC = () => {
                 type="primary"
                 shape="circle"
                 icon={<Icon type="xinzeng" />}
+                onClick={() => handleShowModal(type)}
               />
             </div>
+            {showModal && (
+              <AppModal
+                mode="create"
+                className="empty-modal"
+                type={type}
+                onClose={handleClose}
+                onOk={(name, type) => handleOk(name, type)}
+              />
+            )}
           </div>
         );
       })}
