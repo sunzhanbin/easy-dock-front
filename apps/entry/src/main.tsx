50ddfe4fdf5098acba52caba25e0c65a9fe9b78d
caox <caox@enn.cn>
2021-12-15 16:29:20 +0800

    style: 用户信息和退出登录处理

---
+++ apps/entry/src/main.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "@/reportWebVitals";
+import { store } from "@/store";
+import App from "@/App";
+import "antd/dist/antd.css";
+import "@/index.scss";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
--- apps/entry/src/views/home/index.slice.ts
+++ apps/entry/src/views/home/index.slice.ts
@@ -1,14 +1,20 @@
-import { homeManage } from "@/http";
+import { homeManageBuilder, homeManageRuntime } from "@/http";
 import { createSlice, PayloadAction, current } from "@reduxjs/toolkit";
 import { RootState } from "@/store";
-// import { fetchUser } from '@utils/apis';
+import { User } from "@utils/types";
+import cookie from "js-cookie";
+
+import Auth from "@enc/sso";
+import { axios, runTime } from "@utils/fetch";
 
 export interface HomeManagerState {
+  userInfo: User | null;
   projectId: number; // 当前所属项目ID；
   projectList: { [key: string]: any }[]; // 当前项目List；
 }
 
 const initialState: HomeManagerState = {
+  userInfo: null,
   projectId: 0,
   projectList: [],
 };
@@ -20,27 +26,44 @@ export const HomeManagerSlice = createSlice({
     setProjectId: (state, action: PayloadAction<number>) => {
       state.projectId = action.payload;
     },
-    setProjectList: (
-      state,
-      action: PayloadAction<{ [key: string]: any }[]>
-    ) => {
-      console.log(action.payload, "3333");
-      state.projectList = action.payload;
-    },
   },
   extraReducers: (builder) => {
     builder.addMatcher(
-      homeManage.endpoints.getProjectList.matchFulfilled,
+      homeManageBuilder.endpoints.getProjectList.matchFulfilled,
       (state, action) => {
         state.projectList = action.payload;
         state.projectId = action.payload.length && action.payload[0].id;
       }
     );
+    builder.addMatcher(
+      homeManageRuntime.endpoints.getUserInfo.matchFulfilled,
+      (state, action) => {
+        const { power, user } = action.payload;
+        state.userInfo = {
+          avatar: user.avatar,
+          username: user.userName,
+          id: user.id,
+          power: power,
+        };
+      }
+    );
+    builder.addMatcher(
+      homeManageRuntime.endpoints.logout.matchFulfilled,
+      (state, action) => {
+        console.log(runTime, "ffff");
+        Auth.logout(action.payload ? action.payload : undefined);
+        state.userInfo = null;
+        delete axios.defaults.headers.auth;
+        // 清掉cookie
+        // cookie.remove("token");
+      }
+    );
   },
 });
 
-export const { setProjectId, setProjectList } = HomeManagerSlice.actions;
+export const { setProjectId } = HomeManagerSlice.actions;
 
 export const selectProjectId = (state: RootState) => state.home.projectId;
+export const selectUserInfo = (state: RootState) => state.home.userInfo;
 
 export default HomeManagerSlice.reducer;
