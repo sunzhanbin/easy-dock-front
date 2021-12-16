3f6aaa7586157854798016d7508d73b7ec585455
caox <caox@enn.cn>
2021-12-16 18:37:52 +0800

    chore: 应用编排子应用跳转功能

--- apps/entry/src/utils/utils.ts
+++ apps/entry/src/utils/utils.ts
@@ -1,6 +1,7 @@
 import { Menu } from "@utils/types";
 import { axios } from "@utils/fetch";
 import { AbstractTooltipProps } from "antd/lib/tooltip";
+import { HomeSubAppType, ResponseType } from "@/consts";
 
 export const getPopupContainer: AbstractTooltipProps["getPopupContainer"] = (
   container
@@ -47,4 +48,40 @@ export const imgIdToUrl = async (id: string) => {
   const blob = new Blob([res as any]);
   const url: string = window.URL.createObjectURL(blob);
   return url;
+};
+
+/**
+ * 不同类型子应用跳转
+ * @param type 子应用类型
+ * @param id 子应用id
+ * @param getCanvasId 大屏接口
+ * @param getHolosceneId 空间接口
+ * @constructor
+ */
+export const JumpLinkToUrl = async (
+  type: number,
+  id: number,
+  getCanvasId: (v: number) => Promise<any>,
+  getHolosceneId: (v: number) => Promise<any>
+) => {
+  if (type === HomeSubAppType.CANVAS) {
+    const { data: canvasData }: ResponseType = await getCanvasId(id);
+    if (!canvasData) return;
+    // 大屏跳转需要拼sso=true保证用户信息不丢失
+    window.open(
+      `http://10.19.248.238:28180/dashboard/${canvasData.refId}?sso=true`
+    );
+  } else if (type === HomeSubAppType.SPACE) {
+    const { data: spaceData }: ResponseType = await getHolosceneId(id);
+    if (!spaceData) return;
+    window.open(`http://10.19.248.238:9003/#/scene/${spaceData.refId}`);
+  } else if (type === HomeSubAppType.FLOW) {
+    window.open(
+      `http://10.19.248.238:28303/builder/flow/bpm-editor/${id}/flow-design`
+    );
+  } else if (type === HomeSubAppType.FORM) {
+    window.open(
+      `http://10.19.248.238:28303/builder/flow/bpm-editor/${id}/form-design`
+    );
+  }
 };
