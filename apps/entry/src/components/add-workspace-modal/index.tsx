538abcd485b4df2b6361b393499bbc97858f56e9
caox <caox@enn.cn>
2021-12-16 18:06:48 +0800

    chore: 工作区编辑和删除功能

--- apps/entry/src/components/add-workspace-modal/index.tsx
+++ apps/entry/src/components/add-workspace-modal/index.tsx
@@ -1,15 +1,27 @@
 import React, { useCallback, useState, useImperativeHandle } from "react";
-import { useAppSelector } from "@/store";
-import { useAddWorkspaceMutation } from "@/http";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { useAddWorkspaceMutation, useEditWorkspaceMutation } from "@/http";
 import { selectProjectId } from "@views/home/index.slice";
 import { Modal, Form, Input } from "antd";
 import useMemoCallback from "@common/hooks/use-memo-callback";
 import { nameRule } from "@/consts";
+import {
+  selectCurrentWorkspaceId,
+  setCurrentWorkspaceId,
+} from "@views/app-manager/index.slice";
 
 const AddWorkspaceModal = React.forwardRef(function addWorkspace(_, ref) {
+  const dispatch = useAppDispatch();
   const projectId = useAppSelector(selectProjectId);
+  const workspaceId = useAppSelector(selectCurrentWorkspaceId);
+
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [initialValues, setInitialValues] = useState<{
+    [key: string]: string;
+  }>({});
+  const [title, setTitle] = useState<string>("");
   const [addWorkspace] = useAddWorkspaceMutation();
+  const [editWorkspace] = useEditWorkspaceMutation();
   const [form] = Form.useForm();
 
   useImperativeHandle(ref, () => ({
@@ -19,6 +31,12 @@ const AddWorkspaceModal = React.forwardRef(function addWorkspace(_, ref) {
     hide: () => {
       setIsModalVisible(false);
     },
+    setTitle: (title: string) => {
+      setTitle(title);
+    },
+    setWorkspaceName: (name: string) => {
+      setInitialValues({ name });
+    },
   }));
 
   const handleVisible = useCallback((isVisible: boolean) => {
@@ -26,8 +44,13 @@ const AddWorkspaceModal = React.forwardRef(function addWorkspace(_, ref) {
   }, []);
 
   const handleOk = useMemoCallback(async () => {
-    form.validateFields().then(({ name }) => {
-      addWorkspace({ name, projectId });
+    form.validateFields().then(async ({ name }) => {
+      if (title === "新增") {
+        addWorkspace({ name, projectId });
+      } else {
+        await editWorkspace({ name, id: workspaceId });
+        dispatch(setCurrentWorkspaceId(workspaceId));
+      }
       handleVisible(false);
     });
   });
@@ -38,14 +61,14 @@ const AddWorkspaceModal = React.forwardRef(function addWorkspace(_, ref) {
 
   return (
     <Modal
-      title="新增工作区"
+      title={`${title}工作区`}
       okText="确定"
       cancelText="取消"
       visible={isModalVisible}
       onOk={handleOk}
       onCancel={handleCancel}
     >
-      <Form form={form}>
+      <Form form={form} initialValues={initialValues}>
         <Form.Item name="name" label="工作区名称" required rules={[nameRule]}>
           <Input size="large" placeholder="请输入" />
         </Form.Item>
