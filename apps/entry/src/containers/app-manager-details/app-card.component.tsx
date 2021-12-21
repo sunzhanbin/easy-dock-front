63e4b27d29e6632a75d3659a0d4bc59545be1e45
caox <caox@enn.cn>
2021-12-21 11:04:29 +0800

    chore: 应用编排应用列表样式修改&弹框跳转阻止冒泡

--- apps/entry/src/containers/app-manager-details/app-card.component.tsx
+++ apps/entry/src/containers/app-manager-details/app-card.component.tsx
@@ -186,21 +186,21 @@ const AppCard: FC<AppCardProps> = ({ subApp, className }) => {
     >
       <div className="app-card-base">
         <img src={imageMap[type]} alt="icon" className="app-image" />
-        <div className="name">
-          <Text text={name} />
+        <div>
+          <Text text={name} className="name" />
+          <div className="app-card-more">
+            <div className="info">
+              {version?.version && (
+                <span className="version">{version?.version}</span>
+              )}
+              <span className="type">{typeMap[type]}</span>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="app-card-more">
-        <div className="info">
-          {version?.version && (
-            <span className="version">{version?.version}</span>
-          )}
-          <span className="type">{typeMap[type]}</span>
-        </div>
-        <div className="more" onClick={(e) => handleShowOperation(e)}>
-          <Icon type="gengduo" className="icon" />
-          {showPopup && content}
-        </div>
+      <div className="more" onClick={(e) => handleShowOperation(e)}>
+        <Icon type="gengduo" className="icon" />
+        {showPopup && content}
       </div>
       {statusNode}
       {showModal && (
