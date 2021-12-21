57b0023dd84b576433716848fcb906f39d346f3d
wangjisi <wangjisi@enn.cn>
2021-12-21 16:22:10 +0800

    exclude theme-scheme

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -1,7 +1,7 @@
 import 'antd/dist/antd.css';
-import '@enc/theme-scheme/dist/react/antd/antd.4.17-alpha.6.min.css';
+//import '@enc/theme-scheme/dist/react/antd/antd.4.17-alpha.6.min.css';
 import './styles/base.scss';
-import '@enc/theme-scheme/dist/variable.css';
+//import '@enc/theme-scheme/dist/variable.css';
 
 import { memo } from 'react';
 import { Switch, Route } from 'react-router-dom';
@@ -13,18 +13,18 @@ import BuilderLayout from '@/layouts/builder-layout';
 import RuntimeLayout from '@/layouts/runtime-layout';
 import { ROUTES } from '@consts';
 import cookie from 'js-cookie';
-import { registerTheme } from '@enc/theme-scheme/dist/utils.esm';
+//import { registerTheme } from '@enc/theme-scheme/dist/utils.esm';
 
 Auth.setConfig({ server: window.SSO_LOGIN_URL });
 const query = decodeURIComponent(window.location.href.split('?')[1]);
 const theme = new URLSearchParams(query).get('theme');
 
-if (theme) {
+/* if (theme) {
   cookie.set('theme', theme);
   registerTheme({
     theme,
   });
-}
+} */
 
 function AppEntry() {
   return (
