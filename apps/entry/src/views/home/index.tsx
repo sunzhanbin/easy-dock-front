2a9a031a3c294d19c1ef66e8e2be8eb271df1d9a
caox <caox@enn.cn>
2021-12-31 14:41:46 +0800

    chore: 删除test

--- apps/entry/src/views/home/index.tsx
+++ apps/entry/src/views/home/index.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import { Layout } from "antd";
-import "@views/home/index.style.scss";
-import HomeNewAPP from "@containers/home-manager/new-app-component";
-import HomeWorkspaceList from "@containers/home-manager/workspace-list-component";
-import HomeNewSub from "@containers/home-manager/new-sub-component";
-import HeaderAssetData from "@containers/home-manager/asset-data-component";
-import HeaderHelp from "@containers/home-manager/help-component";
-
-
-console.log('test')
+import React from 'react';
+import { Layout } from 'antd';
+import '@views/home/index.style.scss';
+import HomeNewAPP from '@containers/home-manager/new-app-component';
+import HomeWorkspaceList from '@containers/home-manager/workspace-list-component';
+import HomeNewSub from '@containers/home-manager/new-sub-component';
+import HeaderAssetData from '@containers/home-manager/asset-data-component';
+import HeaderHelp from '@containers/home-manager/help-component';
 
 const { Sider, Content, Footer } = Layout;
 const Start = () => {
@@ -19,8 +16,7 @@ const Start = () => {
         <HeaderAssetData />
         <HeaderHelp />
         <Footer className="footer">
-          Copyright©2018 新智认知数字科技股份有限公司 桂公网安备
-          45050302000074号桂ICP备14001670号-4
+          Copyright©2018 新智认知数字科技股份有限公司 桂公网安备 45050302000074号桂ICP备14001670号-4
         </Footer>
       </Content>
       <Sider className="home_sider">
