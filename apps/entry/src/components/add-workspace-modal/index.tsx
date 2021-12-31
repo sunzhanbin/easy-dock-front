a06ee0631dd43d78ef46808a53f5fca2237ea163
wangchaochuan <wangcci@enn.cn>
2021-12-31 15:41:24 +0800

    test commit

--- apps/entry/src/components/add-workspace-modal/index.tsx
+++ apps/entry/src/components/add-workspace-modal/index.tsx
@@ -55,6 +55,7 @@ const AddWorkspaceModal = React.forwardRef(function AddWorkspace(_, ref) {
 
   const handleCancel = useCallback(() => {
     form.resetFields();
+    console.info(111);
     handleVisible(false);
   }, [handleVisible]);
 
