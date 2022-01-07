a9de2efcb7f922104a695b2c389774acfb6fb422
caox <caox@enn.cn>
2022-01-07 18:26:01 +0800

    chore: 优化代码

--- apps/entry/src/views/main/animate-screen-scroll.tsx
+++ apps/entry/src/views/main/animate-screen-scroll.tsx
@@ -4,36 +4,30 @@ import classnames from "classnames";
 import '@views/main/index.style.scss';
 import anime from "animejs";
 
+const list = new Array(8).fill(0);
+
 const AnimateScreenScroll = () => {
     useEffect(() => {
-        const circle1 = document.querySelector(".circle-1")!;
-        const circle3 = document.querySelector(".circle-3")!;
-        const circle4 = document.querySelector(".circle-4")!;
-        const circle5 = document.querySelector(".circle-5")!;
+        const circleList = document.querySelectorAll(".circle")!;
         const subAppWrapper = document.querySelector(".sub-app-wrapper")!;
-        const circle6 = document.querySelector(".circle-6")!;
-        const circle7 = document.querySelector(".circle-7")!;
         const circle8 = document.querySelector(".circle-8")!;
         anime.timeline({
             duration: 200,
             easing: 'easeInOutSine',
-        }).add({
+        })
+            .add({
             targets: '.circle-8',
             translateX: '685%',
             begin: () => {
-                circle1.classList.add("no-filter");
-                circle7.classList.add("no-filter");
-                circle6.classList.add("no-filter");
-                circle5.classList.add("no-filter");
-                circle4.classList.add("no-filter");
-                circle3.classList.add("no-filter");
-                circle8.classList.add("no-filter");
+                circleList.forEach(circle => {
+                    circle.classList.add("no-filter");
+                })
             },
             complete: (a: any) => {
                 circle8.classList.add("has-text");
                 subAppWrapper.classList.add("sub-fade-in");
             }
-        }, 400)
+            }, 400)
             .add({
                 targets: '.circle-7',
                 translateX: '400%',
@@ -51,23 +45,23 @@ const AnimateScreenScroll = () => {
                 translateX: '150%',
             },  600)
             .add({
-                targets: '.bubble-list-wrapper .circle-3',
+                targets: '.circle-3',
                 translateX: '85%',
             }, 650)
             .add({
-            targets: '.circle-2',
-            translateX: '36%',
-        }, 700)
+                targets: '.circle-2',
+                translateX: '36%',
+            }, 700)
     }, []);
 
     return (
         <>
             <div className="bubble-list-wrapper">
                 {
-                    MAIN_SUB_APP_LIST.map((circle, index) =>
+                    list.map((circle, index) =>
                         <Fragment key={index}>
                             <div className={classnames(`circle-${index+1}`, "circle")}>
-                                {(index + 1 === 8) && <span className="circle-text">创建应用</span>}
+                                {(index + 1 === list.length) && <span className="circle-text">创建应用</span>}
                             </div>
                         </Fragment>
                     )}
