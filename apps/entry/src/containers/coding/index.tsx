a6f4481fe06ac9089e90b71b32318e47b6aae7e9
wangchaochuan <wangcci@enn.cn>
2022-01-10 15:56:49 +0800

    feat:资产中心和模板商城页面UI优化

---
+++ apps/entry/src/containers/coding/index.tsx
@@ -0,0 +1,17 @@
+import { memo, FC } from 'react';
+import codingImage from '@assets/images/coding.png';
+import './index.style.scss';
+
+const Coding: FC<{ title?: string }> = ({ title }) => {
+  return (
+    <div className="coding-container">
+      <div className="image-container">
+        <img src={codingImage} alt="image" className="image" />
+        <div className="title">{title}</div>
+        <div className="coding">正在开发中…</div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(Coding);
