372dcaefa4de9459de51569845b3da65b711df96
wangchaochuan <wangcci@enn.cn>
2022-01-19 17:29:34 +0800

    feat:代码优化

--- apps/entry/src/containers/app-manager-sider/index.tsx
+++ apps/entry/src/containers/app-manager-sider/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Layout, Menu, Button, Input, Dropdown, message } from "antd";
 import { useDeleteWorkspaceMutation, useFetchWorkspaceListQuery } from "@/http";
 import { useAppDispatch } from "@/store";
