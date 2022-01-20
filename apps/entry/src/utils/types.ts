dc8f31fad279afe9886c4ce4b7bd0fdc53a9d581
caox <caox@enn.cn>
2022-01-20 13:15:23 +0800

    feature: 权限设置修改完成

--- apps/entry/src/utils/types.ts
+++ apps/entry/src/utils/types.ts
@@ -15,11 +15,80 @@ export enum RoleEnum {
   PROJECT_MANAGER = 4, //项目管理员
   ADMIN = 8, //超管
 }
+
+export type RevokeAuthParams = {
+  id: number;
+  ownerType: number;
+  power: number;
+};
+
 export enum AuthEnum {
   ADMIN = 1, //管理员权限
   DATA = 2, //数据权限
   VISIT = 4, //访问权限
 }
+
+// 资源类型
+export enum ResourceTypeEnum {
+  PROJECT = 1,
+  APP = 2,
+  SUB_APP = 3,
+}
+
+export enum OwnerTypeEnum {
+  USER = 1, //用户
+  ROLE = 2, //角色
+  DEPARTMENT = 3, //部门
+}
+export type AssignAuthParams = {
+  ownerKey: string;
+  ownerType: number;
+  power: number;
+  resourceKey: string;
+  resourceType: number;
+};
+export type UserOwner = {
+  id: number | string; //登录名
+  avatar?: string; // 头像
+  username?: string; // 中文名
+  userName?: string;
+  name: string;
+};
+export type DepartOwner = {
+  id: number | string;
+  code?: string;
+  name: string;
+  parentId?: number;
+};
+export type RoleOwner = {
+  id: number | string;
+  name: string;
+};
+export type Power = {
+  id: number;
+  owner: UserOwner | DepartOwner | RoleOwner;
+  ownerType: number;
+  resourceKey: string;
+  resourceType: number;
+};
+export type ProjectPower = {
+  createTime: number;
+  id: number;
+  name: string;
+  powers: Power[]; //被授权的对象
+  status: number;
+};
+
+export type SubAppPower = {
+  createTime: number;
+  id: number;
+  name: string;
+  powers: Power[]; //被授权的对象
+  status: -1 | 1; //-1:停用 1:启用
+  type: 1 | 2; //1:大屏类子应用 2:流程类子应用
+  openVisit?: boolean; //所有人可访问
+};
+
 export interface workspaceShape {
   name: string;
   id: number;
