753dafc3e496c0efa9661f66c5783b0222461af6
caox <caox@enn.cn>
2022-01-24 10:08:21 +0800

    chore: 产品要求应用端不需要提供应用端首页入口

--- apps/entry/src/components/header/index.tsx
+++ apps/entry/src/components/header/index.tsx
@@ -6,10 +6,7 @@ import classnames from "classnames";
 import UserComponent from "@components/header/user";
 import "@components/header/index.style.scss";
 import ProjectComponent from "@components/header/project";
-import { RoleEnum } from "@utils/types";
 import NewSubAppPopover from "@components/header/new-subapp-popover.component";
-import { useAppSelector } from "@/store";
-import { selectUserInfo } from "@views/home/index.slice";
 
 interface HeaderProps {
   children?: ReactNode;
@@ -17,21 +14,10 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   const location = useLocation();
-  const userInfo = useAppSelector(selectUserInfo);
-
   // 首页及应用端不展示项目及其他菜单
   const showProject = location.pathname !== "/" && location.pathname !== "/runtime";
   // 工作台不展示新建子应用浮框
   const showPopover = location.pathname !== "/home";
-
-  // 是否有权限跳转到应用端,只有正常项目租户才有权限
-  const canGoApp = useMemo(() => {
-    if (userInfo && userInfo.power) {
-      const { power } = userInfo;
-      return (power & RoleEnum.TENEMENT) === RoleEnum.TENEMENT;
-    }
-    return false;
-  }, [userInfo]);
 
   return (
     <div className={classnames("header_container", location.pathname === "/" ? "main-header" : "")}>
@@ -44,12 +30,6 @@ export default function Header({ children }: HeaderProps) {
         <div className="right">
           {showProject ? (
             <>
-              {canGoApp ? (
-                <a href={`/runtime`} target="_blank" rel="noreferrer" className="appClient">
-                  {/*<Icon type="yingyonduandinglan" className={styles.icon} />*/}
-                  <span className="text">应用端</span>
-                </a>
-              ) : null}
               {showPopover ? <NewSubAppPopover /> : <span className="hidden" />}
               <Icon className="icon" type="shezhi" />
               <Icon className="icon" type="shuoming" />
