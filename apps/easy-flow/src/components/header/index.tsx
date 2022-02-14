f2ed80718c7020e7f52e19fb5ca8b692fc478a9d
caox <caox@enn.cn>
2022-02-14 09:45:19 +0800

    chore: 返回功能去掉

--- apps/easy-flow/src/components/header/index.tsx
+++ apps/easy-flow/src/components/header/index.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, memo, useCallback } from "react";
-import { useHistory } from "react-router-dom";
+import { ReactNode, memo } from "react";
+// import { useHistory } from "react-router-dom";
 import classnames from "classnames";
-import { Icon } from "@common/components";
+// import { Icon } from "@common/components";
 import styles from "./index.module.scss";
 
 interface DetailHeaderProps {
@@ -10,23 +10,23 @@ interface DetailHeaderProps {
   children?: ReactNode;
   className?: string;
   // eslint-disable-next-line @typescript-eslint/ban-types
-  goBack?: Function;
+  // goBack?: Function;
 }
 
 function DetailHeader(props: DetailHeaderProps) {
-  const history = useHistory();
-  const { backText, backClassName, children, className, goBack } = props;
-  const handelClick = useCallback(() => {
-    if (goBack) {
-      goBack();
-    } else {
-      history.goBack();
-    }
-  }, [history, goBack]);
+  // const history = useHistory();
+  const { backText, backClassName, children, className } = props;
+  // const handelClick = useCallback(() => {
+  //   if (goBack) {
+  //     goBack();
+  //   } else {
+  //     history.goBack();
+  //   }
+  // }, [history, goBack]);
   return (
     <div className={classnames(styles.header, className)}>
-      <div className={classnames(styles.back, backClassName)} onClick={handelClick}>
-        <Icon className={styles.icon} type="fanhui" />
+      <div className={classnames(styles.back, backClassName)}>
+        {/*<Icon className={styles.icon} type="fanhui" />*/}
         {backText}
       </div>
       {children}
