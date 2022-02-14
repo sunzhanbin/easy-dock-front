4faaa97681188460458daaf39e82ed671f042ae9
wangchaochuan <wangcci@enn.cn>
2022-02-14 09:41:25 +0800

    fix:fix some bug

--- apps/easy-flow/src/init.ts
+++ apps/easy-flow/src/init.ts
@@ -10,8 +10,8 @@ console.log("__webpack_public_path__: default");
 if (window.__POWERED_BY_QIANKUN__) {
   appConfig.micro = true;
   // eslint-disable-next-line no-undef
-  // const tailPath = window.IS_RELATIVE === true ? "easyflow/" : "/";
-  const tailPath = window.IS_RELATIVE === true ? "/" : "/";
+  const tailPath = window.IS_RELATIVE === true ? "easyflow/" : "/";
+  // const tailPath = window.IS_RELATIVE === true ? "/" : "/";
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ + tailPath; // 替换webpack的ouput.publicPath
   // eslint-disable-next-line no-undef
   appConfig.publicPath = __webpack_public_path__; // 加载public文件夹里的资源使用
