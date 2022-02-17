e1b0f4da8a5e3976aca0b9811aeb076f7e634323
caox <caox@enn.cn>
2022-02-17 10:49:04 +0800

    chore: 流程提交校验必填项时数字控件的value值没有获取到修复

--- apps/easy-flow/src/components/basic-shop/basic-components/InputNumber/index.tsx
+++ apps/easy-flow/src/components/basic-shop/basic-components/InputNumber/index.tsx
@@ -18,6 +18,11 @@ const InputNumberContainer = (props: InputNumberProps & { [key: string]: any }) 
           prop.value = numberValue;
         }
         onChange && onChange(numberValue!);
+      } else if (props?.defaultNumber?.customData === 0) {
+        // 数字控件在1.2.0中需要展示默认值为0进行计算
+        // 由于流程提交校验必填项时数字控件的value值没有获取到  此处暂时手动重置
+        prop.value = 0;
+        onChange && onChange(0);
       }
     }, 0);
 
