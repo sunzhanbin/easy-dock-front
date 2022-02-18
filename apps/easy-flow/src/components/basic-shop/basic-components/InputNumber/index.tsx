e3e87999774fd48c81c4a3720d464d122b8ed59b
caox <caox@enn.cn>
2022-02-18 15:30:50 +0800

    chore: 默认值数值为零 没有设置value值时需要值为0

--- apps/easy-flow/src/components/basic-shop/basic-components/InputNumber/index.tsx
+++ apps/easy-flow/src/components/basic-shop/basic-components/InputNumber/index.tsx
@@ -10,6 +10,8 @@ const InputNumberContainer = (props: InputNumberProps & { [key: string]: any }) 
   const { onChange } = props;
   const propList = useMemo(() => {
     const prop = { ...props };
+    console.log(prop, "*****");
+
     setTimeout(() => {
       if (form && rules && refresh !== undefined) {
         const formValue = form.getFieldsValue();
@@ -18,7 +20,7 @@ const InputNumberContainer = (props: InputNumberProps & { [key: string]: any }) 
           prop.value = numberValue;
         }
         onChange && onChange(numberValue!);
-      } else if (props?.defaultNumber?.customData === 0) {
+      } else if (props?.defaultNumber?.customData === 0 && prop.value === undefined) {
         // 数字控件在1.2.0中需要展示默认值为0进行计算
         // 由于流程提交校验必填项时数字控件的value值没有获取到  此处暂时手动重置
         prop.value = 0;
@@ -26,6 +28,7 @@ const InputNumberContainer = (props: InputNumberProps & { [key: string]: any }) 
       }
     }, 0);
 
+    console.log(prop);
     return prop;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [form, props, refresh, rules]);
