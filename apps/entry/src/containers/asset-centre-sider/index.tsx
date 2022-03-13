c37df2c6953817d9473ac60287fd74be2865e2ef
caox <caox@enn.cn>
2022-03-13 16:52:02 +0800

    feature：路由划分

--- apps/entry/src/containers/asset-centre-sider/index.tsx
+++ apps/entry/src/containers/asset-centre-sider/index.tsx
@@ -1,6 +1,6 @@
-import { memo } from "react";
+import { memo, useState, useEffect } from "react";
 import { Layout, Menu } from "antd";
-import { ASSET_CENTRE_MENU } from "@utils/const";
+import { ASSET_CENTRE_MENU, AssetCentreEnum } from "@utils/const";
 import { Icon } from "@common/components";
 import { useNavigate } from "react-router-dom";
 import useMemoCallback from "@common/hooks/use-memo-callback";
@@ -15,6 +15,13 @@ type menuType = {
 
 const AssetCentreSider = () => {
   const navigate = useNavigate();
+  const [activeMenu, setActiveMenu] = useState<string>("");
+
+  useEffect(() => {
+    navigate(`./${AssetCentreEnum.PLUGINS_MANAGE}`);
+    setActiveMenu(AssetCentreEnum.PLUGINS_MANAGE);
+    // eslint-disable-next-line
+  }, []);
 
   const renderIcon = (icon: string) => {
     return <Icon type={icon} />;
@@ -22,13 +29,14 @@ const AssetCentreSider = () => {
 
   const handleMenuClick = useMemoCallback(({ key }) => {
     navigate(`./${key}`);
+    setActiveMenu(key);
   });
 
   return (
     <div className="asset-centre-sider">
       <Sider trigger={null} collapsible width={240}>
         <div className="menu">
-          <Menu mode="inline" defaultSelectedKeys={["plugins-manage"]} onClick={handleMenuClick}>
+          <Menu mode="inline" selectedKeys={[activeMenu]} onClick={handleMenuClick}>
             {ASSET_CENTRE_MENU.map((menu: menuType) => (
               <Menu.Item key={menu.type} icon={renderIcon(menu?.icon)}>
                 {menu.name}
--- apps/entry/src/views/asset-centre/index.tsx
+++ apps/entry/src/views/asset-centre/index.tsx
@@ -6,15 +6,18 @@ import "@views/asset-centre/index.style.scss";
 import { useAppSelector } from "@/store";
 import { selectIsAdmin } from "@views/home/index.slice";
 import Coding from "@containers/coding";
+import { useLocation } from "react-router-dom";
+import { AssetCentreEnum } from "@utils/const";
 
 const AssetCentre: FC = () => {
+  const location = useLocation();
   const isAdmin = useAppSelector(selectIsAdmin);
   if (!isAdmin) return <Coding title="资产中心" />;
   return (
     <div className="asset-center-container">
       <Layout>
         <AssetCentreSider />
-        <PluginManager />
+        {location.pathname.endsWith(AssetCentreEnum.PLUGINS_MANAGE) && <PluginManager />}
       </Layout>
     </div>
   );
