093ec4cb966e950959a53cd826fc9583dd50b5a4
wangchaochuan <wangcci@enn.cn>
2022-03-22 16:00:18 +0800

    feat:更换深色模式下图标

--- packages/common/components/icon/index.tsx
+++ packages/common/components/icon/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useLocation } from "react-router-dom";
 import classnames from "classnames";
-import "./iconfont";
+import cookie from "js-cookie";
+import "./iconfont-light";
 import "./iconfont-dark";
 import styles from "./index.module.scss";
 
@@ -15,9 +17,18 @@ interface IconProps {
 export default function Icon(props: IconProps) {
   const { type, className, style, onClick } = props;
 
+  const location = useLocation();
+
+  const theme = useMemo<string>(() => {
+    if (location.search) {
+      const params = new URLSearchParams(location.search.slice(1));
+      return params.get("theme") || "light";
+    }
+    return cookie.get("theme") || "light";
+  }, [location.search]);
   return (
     <svg className={classnames(styles.icon, className)} aria-hidden="true" style={style} onClick={onClick}>
-      <use xlinkHref={`#icon-${type}`}></use>
+      <use xlinkHref={theme === "dark" ? `#icon-${type}` : `#custom-icon-${type}`}></use>
     </svg>
   );
 }
