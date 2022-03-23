fb6c9e9575885ae2635bb5afba56af2c02ced750
caox <caox@enn.cn>
2022-03-23 15:09:38 +0800

    fix: 插件提示语修改& 默认公开&启用修改&弹框和提示语层级修改

--- apps/entry/src/containers/plugin-manager/index.tsx
+++ apps/entry/src/containers/plugin-manager/index.tsx
@@ -24,6 +24,7 @@ import {
   selectPluginsList,
   setPluginsList,
   setBindingTenantList,
+  setJSONMeta,
 } from "@views/asset-centre/index.slice";
 import { useAppDispatch, useAppSelector } from "@/store";
 import GroupManageComponent from "@containers/plugin-manager/group-manage-component";
@@ -163,16 +164,6 @@ const PluginManager = () => {
 
   // 确认上传json
   const handleNext = useMemoCallback(async () => {
-    console.log(Object.keys(jsonMeta), "-----");
-    if (!Object.keys(jsonMeta).length) {
-      return message.error("请上传json文件！");
-    }
-    if (!jsonMeta.meta || !jsonMeta.name) {
-      return message.error("插件文件内容错误，请修改后重新上传！");
-    }
-    if (!jsonMeta.code) {
-      return message.error("code不能为空，请修改后重新上传！");
-    }
     if (currentPlugins) {
       try {
         const values = {
@@ -209,6 +200,7 @@ const PluginManager = () => {
         await addPlugins(values).unwrap();
         message.success("新建成功");
       }
+      dispatch(setJSONMeta({}));
       setShowAddModal(false);
       setShowJsonModal(false);
     } catch (e) {
